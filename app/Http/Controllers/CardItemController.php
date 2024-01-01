d2d34934408aa2afc3dd83198469c9dbcdddd550
TASOUFIANE <soufiane.taatalla@gmail.com>
2024-01-01 01:52:32 -0800

    create database and models

---
+++ app/Http/Controllers/CardItemController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CardItem;
+use Illuminate\Http\Request;
+
+class CardItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CardItem $cardItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CardItem $cardItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CardItem $cardItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CardItem $cardItem)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Admin/Shared/Master.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NavBar from './NavBar.vue'
+import TopBar from './TopBar.vue'
+
+</script>
+
+<template>
+     <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <NavBar></NavBar>
+        <TopBar></TopBar>
+        <main class="p-4 md:ml-64 h-auto pt-20">
+            <slot></slot>
+        </main>
+
+
+     </div>
+</template>
